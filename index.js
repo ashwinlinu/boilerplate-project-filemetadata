17318c69aac0affe258880d9ae2117eb60af8987
Ashwin Linu <ashwinl@terawe.com>
2025-05-14 10:52:59 +0530

    completed

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 var app = express();
+
+const storage = multer.memoryStorage(); // Or use diskStorage for saving files
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
